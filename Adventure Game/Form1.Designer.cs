0266193d79f63986e64839b52ceae514203b0b14
zacktorr498 <zacktorr498@SNS-D-130S0B>
2019-11-04 13:29:36 -0500

    Add project files.

---
+++ Adventure Game/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Adventure_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sceneImage = new System.Windows.Forms.PictureBox();
+            this.blueImage = new System.Windows.Forms.PictureBox();
+            this.redImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.ForeColor = System.Drawing.Color.White;
+            this.blueLabel.Location = new System.Drawing.Point(274, 402);
+            this.blueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(257, 23);
+            this.blueLabel.TabIndex = 10;
+            this.blueLabel.Text = "Pass";
+            // 
+            // redLabel
+            // 
+            this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(274, 366);
+            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(257, 23);
+            this.redLabel.TabIndex = 9;
+            this.redLabel.Text = "Enter";
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Freefrm721 Blk BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.Gold;
+            this.outputLabel.Location = new System.Drawing.Point(65, 25);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(624, 105);
+            this.outputLabel.TabIndex = 8;
+            this.outputLabel.Text = "you are going on a walk through a forest and you come across some old ruins.";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(54, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(649, 131);
+            this.label1.TabIndex = 12;
+            // 
+            // sceneImage
+            // 
+            this.sceneImage.BackgroundImage = global::Adventure_Game.Properties.Resources.Old_Ruins;
+            this.sceneImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.sceneImage.Location = new System.Drawing.Point(176, 143);
+            this.sceneImage.Name = "sceneImage";
+            this.sceneImage.Size = new System.Drawing.Size(381, 208);
+            this.sceneImage.TabIndex = 11;
+            this.sceneImage.TabStop = false;
+            // 
+            // blueImage
+            // 
+            this.blueImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("blueImage.BackgroundImage")));
+            this.blueImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.blueImage.Location = new System.Drawing.Point(231, 392);
+            this.blueImage.Margin = new System.Windows.Forms.Padding(2);
+            this.blueImage.Name = "blueImage";
+            this.blueImage.Size = new System.Drawing.Size(39, 32);
+            this.blueImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.blueImage.TabIndex = 7;
+            this.blueImage.TabStop = false;
+            // 
+            // redImage
+            // 
+            this.redImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("redImage.BackgroundImage")));
+            this.redImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.redImage.Location = new System.Drawing.Point(231, 356);
+            this.redImage.Margin = new System.Windows.Forms.Padding(2);
+            this.redImage.Name = "redImage";
+            this.redImage.Size = new System.Drawing.Size(39, 32);
+            this.redImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.redImage.TabIndex = 6;
+            this.redImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sceneImage);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.blueImage);
+            this.Controls.Add(this.redImage);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.sceneImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blueImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox sceneImage;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox blueImage;
+        private System.Windows.Forms.PictureBox redImage;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Adventure Game/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adventure_Game
+{
+    public partial class Form1 : Form
+    {
+        int scene = 1;
+
+        Random randGen = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.V)       //yellow button press
+            {
+                if (scene == 1) { scene = 2; }
+                else if (scene == 2) { scene = 3; }
+                else if (scene == 3) { scene = 5; }
+                else if (scene == 5) { scene = 7; }
+                else if (scene == 8) { scene = 9; }
+                else if (scene == 10) { scene = 11; }
+                else if (scene == 13) { scene = 14; }
+                else if (scene == 14) { scene = 16; }
+                else if (scene == 16) { scene = 18; }
+
+
+                // other pathways with (letters)
+                else if (scene == 6) { scene = 13; } // = A
+                else if (scene == 7) { scene = 13; } // = A
+                else if (scene == 11) { scene = 13; } // = A
+                else if (scene == 9) { scene = 8; } // = B
+                else if (scene == 19) { scene = 16; } // = C
+                else if (scene == 4) { scene = 8; } // = D
+
+            }
+            else if (e.KeyCode == Keys.Space)  //green button press
+            {
+                if (scene == 1) { scene = 13; }
+                else if (scene == 2) { scene = 4; }
+                else if (scene == 3) { scene = 6; }
+                else if (scene == 5) { scene = 8; }
+                else if (scene == 8) { scene = 10; }
+                else if (scene == 10) { scene = 12; }
+                else if (scene == 13) { scene = 15; }
+
+
+                // other pathways with (letters)
+                else if (scene == 12) { scene = 13; } // = A
+
+
+                //Pathways that lead to the end
+                else if (scene == 15) { scene = 99; } // = end
+                else if (scene == 17) { scene = 99; } // = end
+                else if (scene == 19) { scene = 99; } // right = end
+                else if (scene == 18) { scene = 98; } // leads to the end with some extra text
+                else if (scene == 98) { scene = 99; } // leads to the end with some extra text
+                else if (scene == 9) { scene = 99; } // right = end
+
+
+            }
+
+            //switch
+
+            switch (scene)
+            {
+                
+                case 1:
+
+                    sceneImage.BackgroundImage = Properties.Resources.Old_Ruins;
+                    outputLabel.Text = "you are going on a walk through a forest and you come across some old ruins.";
+                    redLabel.Text = "Enter";
+                    blueLabel.Text = "Pass";
+                    break;
+                case 2: 
+                    outputLabel.Text = "You enter the ruins. there is a doorway to the left and a stair case going down to the right. ";
+                    redLabel.Text = "Left";
+                    blueLabel.Text = "Right";
+                    break;
+                case 3:
+                    outputLabel.Text = "You find a glass case with a key inside. its chained shut but there is enough space for you to fit you hand through.";
+                    redLabel.Text = "Grab it";
+                    blueLabel.Text = "Leave it";
+                    break;
+                case 4:
+                    outputLabel.Text = "You walk down the stairs to a group of spiders and a locked door.";
+                    redLabel.Text = "";
+                    blueLabel.Text = "press space to continue";
+                    break;
+                case 5:
+                    outputLabel.Text = "you slip your hand through the case and grab the key. " +
+                        "There is nothing else in the room so you leave. Now back at the 2 doorways.";
+                    redLabel.Text = "Leave";
+                    blueLabel.Text = "Go down stairs";
+                    break;
+                case 6:
+                    outputLabel.Text = "There is nothing else in the room so you leave and your back in front of the 2 doors";
+                    redLabel.Text = "leave";
+                    blueLabel.Text = "down";
+                    break;
+                case 7:
+                    outputLabel.Text = "You leave the old ruins and come across a cave";
+                    redLabel.Text = "";
+                    blueLabel.Text = "press space to continue";
+                    break;
+                case 8:
+                    outputLabel.Text = "You walk down the stairs and see some spiders up ahead. there is a room to your left.";
+                    redLabel.Text = "Attempt to fight";
+                    blueLabel.Text = "Check out the room ";
+                    break;
+                case 9:
+                    outputLabel.Text = "You die trying to fight the spiders with your bare hands";
+                    redLabel.Text = "Retry";
+                    blueLabel.Text = "Quit";
+                    break;
+                case 10:
+                    sceneImage.BackgroundImage = Properties.Resources.Old_Armory;
+                    outputLabel.Text = "This room seems to be an old armory. There is a rusty sword and slightly rusty axe on the wall. " +
+                        "you grab them because you think they will come in handy. You leave the room after having a look around";
+                    redLabel.Text = "Fight";
+                    blueLabel.Text = "Leave";
+                    break;
+                case 11:
+                    outputLabel.Text = "You kill all the spiders but the sword is now chipped. You see a chest behind a bunch of cobwebs" +
+                        "You cut apart the cobwebs to uncover a big chest with a lock on it. " +
+                        "You unlock it with the key to revel a giant, shiny sword. after looking at the sword, you take it, " +
+                        "leaving the axe and the sword you had and then leave the ruins.";
+                    redLabel.Text = "";
+                    blueLabel.Text = "press space to continue";
+                    break;
+                case 12:
+                    sceneImage.BackgroundImage = Properties.Resources.Cave;
+                    outputLabel.Text = "You leave the ruins and come up onto a cave";
+                    redLabel.Text = "";
+                    blueLabel.Text = "press space to continue";
+                    break;
+                case 13:
+                    sceneImage.BackgroundImage = Properties.Resources.Cave;
+                    outputLabel.Text = "You decide not to enter the ruins and find yourself in front of a cave. Do you enter?";
+                    redLabel.Text = "Enter";
+                    blueLabel.Text = "Pass";
+                    break;
+                case 14:
+                    sceneImage.BackgroundImage = Properties.Resources.Cave_Bear_Treasure;
+                    outputLabel.Text = "You enter the cave and you see a big shining chest behind a giant bear.";
+                    redLabel.Text = "Fight";
+                    blueLabel.Text = "Leave";
+                    break;
+                case 15:
+                    sceneImage.BackgroundImage = Properties.Resources.City;
+                    outputLabel.Text = "you continue your walk and eventually end up back in the city";
+                    redLabel.Text = "";
+                    blueLabel.Text = "press space to continue";
+                    break;
+                case 16:
+                    sceneImage.BackgroundImage = Properties.Resources.Cave_Bear_Treasure;
+                    outputLabel.Text = "You approach the bear slowly. Do you have a weapon to fight the bear?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 17:
+                    sceneImage.BackgroundImage = Properties.Resources.City;
+                    outputLabel.Text = "You left the cave and eventually made your way back into the city";
+                    redLabel.Text = "";
+                    blueLabel.Text = "press space to continue";
+                    break;
+                case 18:
+                    sceneImage.BackgroundImage = Properties.Resources.Treasure_Chest;
+                    outputLabel.Text = "You ready you sword you obtained from the ruins. " +
+                        "After a good few minutes of fighting the bear falls over onto it's side with a bunch of stab wounds and a giant gash on its belly." +
+                        "You go and claim you reward from the giant shining chest. Inside you find tons of gold." +
+                        "You eventually make your way back to the city with your rewards. ";
+                    redLabel.Text = "";
+                    blueLabel.Text = "press space to continue";
+                    break;
+                case 19:
+                    outputLabel.Text = "You didn't have a weapon to fight the bear so you decided to try fighting it with your bare hands and got teared apart.";
+                    redLabel.Text = "Retry";
+                    blueLabel.Text = "Quit";
+                    break;
+                case 98:
+                    outputLabel.Text = "Good Job Hero! What other Adventures Await You? :) ";
+                    redLabel.Text = "";
+                    blueLabel.Text = "press space to continue";
+                    break;
+                case 99:
+                    outputLabel.Text = "Thanks for playing";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
